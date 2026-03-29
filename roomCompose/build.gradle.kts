3a3610bc1419e83c4b0e7fd4ee706b2571733286
kimuratakuou <ruia1082halfnc@gmail.com>
2026-03-29 10:44:52 +0900

    パッケージ名修正

--- roomCompose/build.gradle.kts
+++ roomCompose/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.takaobrog.demotaskapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.takaobrog.demotaskapp"
         minSdk = 35
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,5 +20,5 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "DemoTaskApp"
-include(":app")
+include(":roomCompose")
  
