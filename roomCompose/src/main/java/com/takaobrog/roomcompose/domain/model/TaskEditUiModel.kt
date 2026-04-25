61465b0bf14a717d925a763eceef5e77b95790c8
kimuratakuou <ruia1082halfnc@gmail.com>
2026-04-25 13:10:50 +0900

    GetTaskEditUseCase作成

---
+++ roomCompose/src/main/java/com/takaobrog/roomcompose/domain/model/TaskEditUiModel.kt
@@ -0,0 +1,8 @@
+package com.takaobrog.roomcompose.domain.model
+
+data class TaskEditUiModel(
+    val uid: Int,
+    val title: String,
+    val progressPercent: Float,
+    val targetDate: String?,
+)
