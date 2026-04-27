dc59a49585c7dcbf00aaec2c08a96a63dff5d4b6
kimuratakuou <ruia1082halfnc@gmail.com>
2026-04-28 06:43:53 +0900

    タスク削除ダイアログ作成

--- roomCompose/src/main/java/com/takaobrog/roomcompose/presentation/screen/task_detail/ui_model/TaskDetailEvent.kt
+++ roomCompose/src/main/java/com/takaobrog/roomcompose/presentation/screen/task_detail/ui_model/TaskDetailEvent.kt
@@ -1,7 +1,9 @@
 package com.takaobrog.roomcompose.presentation.screen.task_detail.ui_model
 
 sealed class TaskDetailEvent {
-    data object OnDeleteTaskEvent : TaskDetailEvent()
+    data object OnDeleteConfirmClick : TaskDetailEvent()
+    data object OnDeleteExecute : TaskDetailEvent()
+    data object OnDeleteDismiss : TaskDetailEvent()
     data class OnEditTaskEvent(val uid: Int) : TaskDetailEvent()
     data object OnBackEvent : TaskDetailEvent()
 }
